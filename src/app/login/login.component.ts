fac32254f28910b3676674b70147e5ff7e21fe05
nilesh.devdas@vinsys.com <nilesh.devdas@vinsys.com>
2020-07-14 12:11:39 +0530

    Observable RXJS

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { LoginService } from '../login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,7 +9,7 @@ import { User } from '../user';
 export class LoginComponent implements OnInit {
   loggedin;
   user: User = new User();
-  constructor() { }
+  constructor(public loginService: LoginService) { }
   ngOnInit() {
     this.loggedin = sessionStorage.getItem('loginUser');
   }
@@ -17,12 +18,18 @@ export class LoginComponent implements OnInit {
     console.log(this.user.password);
     sessionStorage.setItem('loginUser', JSON.stringify(this.user));
     this.loggedin = this.user;
+    // you have logged in you need to tell the other components that youhave logged so broadcast .... 
+    // this is what is going to be the next () the next event.....
+    this.loginService.getPublisher().next(this.user);
   }
 
   handleLogout() {
-    console.log("logging out")
+    console.log('logging out')
     sessionStorage.removeItem('loginUser');
     this.loggedin = null;
+    // broadcast you have logged out
+    // on logout alos the next event
+    this.loginService.getPublisher().next(null);
   }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchmoviesService } from '../fetchmovies.service';
 import { CommunicatorService } from '../communicator.service';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,9 +11,13 @@ import { CommunicatorService } from '../communicator.service';
 export class NavbarComponent implements OnInit {
   a = 10;
   loggedin;
-  constructor(public fms: FetchmoviesService, public cms: CommunicatorService) {
+  constructor(public loginService: LoginService) {
   }
   ngOnInit() {
-    this.loggedin = sessionStorage.getItem('loginUser')
+    this.loggedin = sessionStorage.getItem('loginUser');
+    // i want to listen on the events call  loginService --Subscriber events ---->
+    this.loginService.getSubscriber().subscribe((data) => {
+      this.loggedin = data;
+    });
   }
 }
